648a9d997987e9fbd37deb5b0c7a5793e418fc9d
Riyas230195 <ahmedriyas231@gmail.com>
2023-04-08 20:25:28 +0530

    secondgit

---
+++ src/org/github/ClassA.java
@@ -0,0 +1,35 @@
+package org.github;
+
+public class ClassA {
+	
+
+		
+		private void employeeID() {
+	        System.out.println("Employee ID: 2301");
+		}
+		
+		private void employeeName() {
+			System.out.println("Employee name: Ram");
+		}
+		
+		private void employeeAddress() {
+			System.out.println("Employee address: OMR");
+		}
+		
+		private void employeePhone() {
+			System.out.println("Employee phone: 9876543210");
+		}
+		
+		public static void main(String[] args) {
+			
+			ClassA a = new ClassA();
+			a.employeeAddress();
+			a.employeeID();
+			a.employeeName();
+			a.employeePhone();
+		}
+
+	}
+
+
+
